2d1eaef5ea0a675fcd59ea0e9f7e05b0391646d3
Ivan Batuev <bi@get-net.ru>
2021-08-10 17:46:57 +0500

    Add storage

--- frontend/src/store/local.js
+++ frontend/src/store/local.js
@@ -3,7 +3,84 @@ import local from '../services/local.api';
 
 export default {
     namespaced: true,
+    state:      {
+        instrList: []
+    },
+    mutations:  {
+        instrList: []
+    },
     actions:    {
-        
+        getInstrList(ctx, payload) {
+            return local.get('/instr/list', {params: payload})
+                .then(({data}) => {
+                    if (data.result) {
+                        return data.data
+                    } else {
+                        console.log(data);
+                        ctx.dispatch('showError', data, {root:true});
+                    }
+                })
+                .catch(e => ctx.dispatch('showError', e, {root:true}))
+        },
+        addInstr(ctx, payload) {
+            return local.post('/instr/add', payload)
+            .then(({data}) => {
+                if (data.result) {
+                    return data.data
+                } else {
+                    console.log(data);
+                    ctx.dispatch('showError', data, {root:true});
+                }
+            })
+            .catch(e => ctx.dispatch('showError', e, {root:true}))
+        },
+        updatInstr(ctx, payload) {
+            return local.post('/instr/update', payload)
+            .then(({data}) => {
+                if (data.result) {
+                    return data.data
+                } else {
+                    console.log(data);
+                    ctx.dispatch('showError', data, {root:true});
+                }
+            })
+            .catch(e => ctx.dispatch('showError', e, {root:true}))
+        },
+        enableInstr(ctx, payload) {
+            return local.post('/instr/enable', payload)
+            .then(({data}) => {
+                if (data.result) {
+                    return data.data
+                } else {
+                    console.log(data);
+                    ctx.dispatch('showError', data, {root:true});
+                }
+            })
+            .catch(e => ctx.dispatch('showError', e, {root:true}))
+        },
+        disableInstr(ctx, payload) {
+            return local.post('/instr/disable', payload)
+            .then(({data}) => {
+                if (data.result) {
+                    return data.data
+                } else {
+                    console.log(data);
+                    ctx.dispatch('showError', data, {root:true});
+                }
+            })
+            .catch(e => ctx.dispatch('showError', e, {root:true}))
+        },
+        statInstr(ctx, payload) {
+            return local.post('/instr/stat', payload)
+            .then(({data}) => {
+                if (data.result) {
+                    return data.data
+                } else {
+                    console.log(data);
+                    ctx.dispatch('showError', data, {root:true});
+                }
+            })
+            .catch(e => ctx.dispatch('showError', e, {root:true}))
+        }
     }
 }
